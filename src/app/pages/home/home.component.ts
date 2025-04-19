c9c4df7af0d2ec2bc07b1f282023ffdc4b48c78c
DiogoWi <diogowillian499@gmail.com>
2025-04-19 14:52:45 -0300

    adição do home-title

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { HomeTitleComponent } from '../../components/home-title/home-title.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HomeTitleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
