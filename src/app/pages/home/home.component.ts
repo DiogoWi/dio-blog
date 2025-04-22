cfd8cfa126d771c5908ad337529af848e54d4e8c
DiogoWi <diogowillian499@gmail.com>
2025-04-22 15:39:45 -0300

    adição do componente big-card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { HomeTitleComponent } from '../../components/home-title/home-title.component';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, HomeTitleComponent],
+  imports: [HeaderComponent, HomeTitleComponent, BigCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
